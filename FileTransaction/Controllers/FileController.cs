90118d1a5016cb49a70163b929df25fd88ff55c1
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2023-05-05 14:20:19 +0200

    moved logic to a helper class

--- FileTransaction/Controllers/FileController.cs
+++ FileTransaction/Controllers/FileController.cs
@@ -1,3 +1,4 @@
+using FileTransaction.Helpers;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,39 +13,10 @@ namespace FileTransaction.Controllers
         [Route("/processFile")]
         public async Task<IActionResult> ProcessFile(IFormFile file)
         {
-            if (!file.ContentType.StartsWith("text")) return BadRequest("Wrong file format, please upload a .txt file");
-            if (file.Length > 0)
-            {
-                try
-                {
-                    var watch = System.Diagnostics.Stopwatch.StartNew();
-                    var result = 0;
+            var response = await FileHelper.ProcessFile(file);
 
-                    using (var reader = new StreamReader(file.OpenReadStream()))
-                    {
-                        while (reader.Peek() >= 0)
-                        {
-                            var row = await reader.ReadLineAsync();
-                            var rowAsNumber = int.Parse(row.Trim());
-
-                            result += rowAsNumber;
-                        }
-                    }
-
-                    using (var fileWriter = new StreamWriter("Result.txt"))
-                    {
-                        fileWriter.WriteLine($"Sum of integers is: {result}");
-                    }
-
-                    watch.Stop();
-                    var elapsedMs = watch.ElapsedMilliseconds;
-
-                    return Ok($"Sum is: {result}. Processing took about {elapsedMs} milliseconds.");
-                }
-                catch { return BadRequest("Error while calculating the sum. Please check and reupload the file, then try again"); }
-
-            }
-            else return BadRequest("File seems to be empty");
+            if (response.IsOk) return Ok(response.Message);
+            else return BadRequest(response.Message);
         }
 
         [HttpGet]
---
+++ FileTransaction/Helpers/FileHelper.cs
@@ -0,0 +1,47 @@
+﻿using FileTransaction.Models;
+
+namespace FileTransaction.Helpers
+{
+    public static class FileHelper
+    {
+        public static async Task<Response> ProcessFile(IFormFile file)
+        {
+            if (!file.ContentType.StartsWith("text")) return new Response { IsOk = false, Message = "Wrong file format, please upload a .txt file" };
+            if (file.Length > 0)
+            {
+                try
+                {
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+                    var result = 0;
+
+                    using (var reader = new StreamReader(file.OpenReadStream()))
+                    {
+                        while (reader.Peek() >= 0)
+                        {
+                            var row = await reader.ReadLineAsync();
+
+                            if (string.IsNullOrEmpty(row)) continue;
+
+                            var rowAsNumber = int.Parse(row!.Trim());
+
+                            result += rowAsNumber;
+                        }
+                    }
+
+                    using (var fileWriter = new StreamWriter("Result.txt"))
+                    {
+                        fileWriter.WriteLine($"Sum of integers is: {result}");
+                    }
+
+                    watch.Stop();
+                    var elapsedMs = watch.ElapsedMilliseconds;
+
+                    return new Response { IsOk = true, Message = $"Sum is: {result}. Processing took about {elapsedMs} milliseconds." };
+                }
+                catch { return new Response { IsOk = false, Message = "Error while calculating the sum. Please check and reupload the file, then try again" }; };
+
+            }
+            else return new Response { IsOk = false, Message = "File seems to be empty" };
+        }
+    }
+}
